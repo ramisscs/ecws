568b3e065ca3195804da7c46ad9056b677b785c3
ramisscs <rami@sabahalsalem.com.kw>
2026-05-31 11:56:51 +0300

    Switch to SQLite for deployment

--- config/database.php
+++ config/database.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Str;
 
 return [
-    'default' => env('DB_CONNECTION', 'mysql'),
-
+    'default' => env('DB_CONNECTION', 'sqlite'),
+    
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
@@ -16,18 +16,18 @@ return [
             'journal_mode' => null,
             'synchronous' => null,
         ],
-
+        
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'ecws'),
+            'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
@@ -37,18 +37,17 @@ return [
             ]) : [],
         ],
     ],
-
+    
     'migrations' => [
         'table' => 'migrations',
         'update_date_on_publish' => true,
     ],
-
+    
     'redis' => [
         'client' => env('REDIS_CLIENT', 'phpredis'),
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-            'persistent' => env('REDIS_PERSISTENT', false),
         ],
         'default' => [
             'url' => env('REDIS_URL'),
